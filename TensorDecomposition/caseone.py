bdc3aa0efa9b47f209f2522fca72cd9b87f4d59a
M.Emin Ozturk <ozturk.27@osu.edu>
2020-11-25 13:52:34 -0700

    update

--- TensorDecomposition/caseone.py
+++ TensorDecomposition/caseone.py
@@ -31,3 +31,40 @@ TC  =  torch.randn(C_k, C_i, C_j)
 TD  =  torch.randn(D_k, D_i, D_j)
 
 
+print("Matrix Contraction of A1 and A2")
+
+#Calculate A1A2
+
+TA1TA2 = torch.einsum('ij, kjr -> ikr' , [TA1, TA2])
+
+print("Size of A1A2")
+print(TA1TA2.size())
+
+#Calculate A1A2*C
+
+TA1TA2TC = torch.einsum( 'kij, kab -> ijab ' , [TA1TA2 , C])
+
+
+print("Size of A1A2C")
+print(TA1TA2TC.size())
+
+#Calculate A3B
+
+TA3TB = torch.einsum('ij,kjl -> ikl' , [TA3, TB])
+
+print("Size of TA3TB")
+print(TA3TB.size())
+
+#Calculate A3B*D
+
+TA3TBTD = torch.einsum('kij, kab -> ijab' , [TA3TB, TD])
+
+print("Size of TA3TBTD")
+print(TA3TBTD.size())
+
+#Calculate  A1A2C*A3BD
+
+TA1TA2TCTA3TBTD = torch.einsum('abcd, cdij -> abij' , [TA1TA2TC , TA3TBTD])
+
+print("Size of TA3TBTD")
+print(TA3TBTD.size())
